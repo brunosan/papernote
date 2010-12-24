a93fd6575c5858bd5dd2bb6eb491849900bf492b
brunosan <brunosan@gmail.com>
2010-12-24 17:44:55 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Papernote::Application.config.secret_token = 'f3f7571eb2c8fc2e7e8ea6fae058d7a9564ab857f0502af6062426e9da23adeae62117a9331a9c85e9e36bbb7d72a23be3f0b9a32af2a3c418994bcbe8917e4d'
