ab6f0f4f390fdfed1c5f267ab0831280043c2187
brunosan <brunosan@gmail.com>
2010-12-25 20:00:46 +0100

    Static pages basis done..

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+	it "should have right title" do
+	  get 'home'
+      response.should have_selector("title",
+                    :content => "Papernote | Home")
+	end
   end
 
   describe "GET 'contact'" do
@@ -14,6 +20,11 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+	it "should have right title" do
+	  get 'home'
+      response.should have_selector("title",
+                    :content => "Papernote | Contact")
+	end
   end
 
   describe "GET 'about'" do
@@ -21,6 +32,11 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+	it "should have right title" do
+	  get 'home'
+      response.should have_selector("title",
+                    :content => "Papernote | About")
+	end
   end
 
 end
