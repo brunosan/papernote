6ee87d6e48ec18a7c46bd5723a1af46c51a0405e
brunosan <brunosan@gmail.com>
2010-12-27 01:49:09 +0100

    production tokens

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '180851825267100', '05d6c61691c3cc85a21f6c4f9d7311ad' if Rails.env.development?
   provider :facebook, '129558937105012', 'a1d677ac418af76d1d86e571c6047d38' if Rails.env.production?
   
-  end
 end
